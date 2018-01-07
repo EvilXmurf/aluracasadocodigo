6d4075eff77edd9ee1546dda15ceca93126d4561
EvilXmurf <evilxmurf@gmail.com>
2018-01-07 16:17:32 -0200

    Alteração no modo de acesso
    Alteração no modo de acesso

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -2,12 +2,11 @@ var mysql  = require('mysql');
 
 function createDBConnection() {
 
-    if(!process.env.NODE_ENV) {
+    if (!process.env.NODE_ENV || process.env.node === 'dev') {
         return mysql.createConnection({
-                host:'localhost',
-                user:'root',
-                password:'',
-                database:'casadocodigo_nodejs'
+            host: 'localhost',
+            user: 'root',
+            database: 'casadocodigo_nodejs'
         });
     }
 
@@ -17,6 +16,17 @@ function createDBConnection() {
                 user:'root',
                 password:'',
                 database:'casadocodigo_nodejs_test'
+        });
+    }
+
+    if (process.env.NODE_ENV == 'production') {
+        var url = process.env.CLEARDB_DATABASE_URL;
+        var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+        return mysql.createConnection({
+            host:grupos[3],
+            user:grupos[1],
+            password:grupos[2],
+            database:grupos[4]
         });
     }
 }
